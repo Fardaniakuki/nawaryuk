e058863035420125de5159f25524b593e1f7d297
Fardan <fardanrasyah01@gmail.com>
2026-03-29 16:12:06 +0700

    Fix: Use process.cwd for database path

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: path.join(__dirname, '..', '..', 'database.sqlite'),
+    // process.cwd() = Folder paling luar project kamu
+    // Lalu masuk ke folder 'backend' dan cari 'database.sqlite'
+    storage: path.join(process.cwd(), 'backend', 'database.sqlite'),
     logging: false,
 });
 
